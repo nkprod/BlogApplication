1cac102279e436c7f9637558a784abd101eadf0b
Nurlybek Karshiga <nurlybekkarshiga@gmail.com>
2020-09-13 17:20:12 -0400

    Initial setup for the blog application

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,42 @@
+import React, { useContext } from "react";
+import { FlatList, View, Text, StyleSheet, Button } from "react-native";
+import { Context } from "../context/BlogContext";
+import { Feather } from "@expo/vector-icons";
+
+const IndexScreen = () => {
+  const { state, addBlogPost } = useContext(Context);
+  return (
+    <View>
+      <Button title="Add new post" onPress={addBlogPost} />
+      <FlatList
+        data={state}
+        keyExtractor={(blogPost) => blogPost.title}
+        renderItem={({ item }) => (
+          <View style={styles.row}>
+            <Text style={styles.title}>{item.title}</Text>
+            <Feather style={styles.icon} name="trash" color="black" />
+          </View>
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: "grey",
+  },
+  title: {
+    fontSize: 18,
+  },
+  icon: {
+    fontSize: 24,
+  },
+});
+
+export default IndexScreen;
